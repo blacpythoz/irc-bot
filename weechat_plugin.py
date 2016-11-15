5942c8c223f1ae7bfad6eda357f9855976b290db
blacpythoz <blacpythoz@gmail.com>
2016-11-15 09:34:38 +0000

    Added weechat plugin to execute anything

---
+++ weechat_plugin.py
@@ -0,0 +1,53 @@
+import weechat
+import subprocess
+## Testing the functions
+weechat.prnt("",",  from python script!")
+
+SCRIPT_NAME = "Testing"
+SCRIPT_AUTHOR = "Subash"
+SCRIPT_VERSION = "0.1"
+SCRIPT_LICENSE = "Free"
+SCRIPT_DESC = "Execute any commands"
+
+# Your nick in irc
+nick = "blacpythoz"
+
+# Var
+weechat.register(
+        SCRIPT_NAME,
+        SCRIPT_AUTHOR,
+        SCRIPT_VERSION,
+        SCRIPT_LICENSE,
+        SCRIPT_DESC,
+        "",""
+)
+
+def playFile(data,signal,signal_data):
+    message=signal_data.split("PRIVMSG")[1].split(":",1)[1]
+    msg = message.split(" ")
+    if msg[0][0]== '>' and len(msg) == 3:
+        if msg[0] == '>play' and msg[2] ==nick: 
+            ##Debugging
+            weechat.prnt("",message)
+            weechat.prnt("",msg[0])
+            weechat.prnt("",msg[1])
+            weechat.prnt("",msg[2])
+            try:
+                weechat.prnt("","Playing with vlc")
+                subprocess.Popen(["vlc",msg[1]],stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+            except:
+                weechat.prnt("","Error: Occurred")
+        if msg[0] == '>url' and msg[2] ==nick: 
+            try:
+                weechat.prnt("","Playing with web-browser")
+                subprocess.Popen(["xdg-open",msg[1]],stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+            except:
+                weechat.prnt("","Error: Occurred")
+        else:
+            server = signal.split(",")[0]
+            channel = signal_data.split(":")[-1]
+            buffer = weechat.info_get("irc_buffer", "%s,%s" % (server,channel))
+            weechat.prnt(buffer,"Enter in form >play [url] [nickName]")
+    return weechat.WEECHAT_RC_OK
+
+weechat.hook_signal("*,irc_in_privmsg","playFile","")
