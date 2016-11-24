f780c90a30ff268cd8575e354d3bbe2aafdfd7d0
blacpythoz <blacpythoz@gmail.com>
2016-11-24 23:26:00 +0000

    Added setup info

--- setup.py
+++ setup.py
@@ -1,1 +1,14 @@
+from distutils.core import setup
+
+setup(name='bot3',
+      version='1.0.1',
+      description='Simple irc bot written in python',
+      author='Subash Aryc',
+      author_email='blacpythoz@gmail.com',
+      url='https://github.com/blacpythoz/',
+      py_modules=['bot3']
+      scripts=['bot3']
+      data_files=['bot.config']
+      
+      )
 
